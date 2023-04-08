4e5de31f494a57f34a8841e49c60abca967d5fc9
pira99 <piraveen@live.nl>
2023-04-08 14:25:41 +0200

    Empty Gui for the simulator

---
+++ project/main.py
@@ -0,0 +1,18 @@
+import tkinter
+import customtkinter
+
+# System appearance settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# App frame settings
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Cybersecurity Simulator")
+
+# User Interface elements
+title = customtkinter.CTkLabel
+
+
+# Run app
+app.mainloop()
